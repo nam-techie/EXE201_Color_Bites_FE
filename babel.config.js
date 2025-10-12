513bd3653ff7a39abba9b260dbed127b429577ee
nam-techie <namdpse180259@fpt.edu.vn>
2025-10-12 17:19:40 +0700

    Refactor babel.config.js for improved formatting and consistency
    - Adjusted indentation and spacing for better readability.
    - Ensured proper structure of the configuration object.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = function (api) {
-   api.cache(true)
-   return {
-     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-     plugins: [
-       'react-native-reanimated/plugin' // Thêm dòng này
-     ]
-   }
- }
+  api.cache(true)
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins: [
+      'react-native-reanimated/plugin' // Thêm dòng này
+    ]
+  }
+}
