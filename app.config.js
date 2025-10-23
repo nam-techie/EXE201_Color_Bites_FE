2db75b5f626ce1b2ad838173a46463fca1b8ca0a
nam-techie <namdpse180259@fpt.edu.vn>
2025-10-23 09:24:37 +0700

    Update splash screen icon path in app.config.js to use the new icon format.

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ export default ({ config }) => ({
       [
         'expo-splash-screen',
         {
-          image: './assets/images/splash-icon.png',
+          image: './assets/images/icon.png',
           imageWidth: 200,
           resizeMode: 'contain',
           backgroundColor: '#ffffff',
